506778013528fe7f741722dddc2fbb7247b6a2b2
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2022-12-26 21:11:50 +0100

    Give a UUID to the machine

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -138,7 +138,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "6c39942d-b709-4b1a-835c-ab7e4c46736c"
 
 // @section stepper drivers
 
